dc82a9b6430fe6f60a4f90e1acba75305cc7b988
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-09-04 14:41:10 +0300

    create a custom user manager

--- core/models.py
+++ core/models.py
@@ -1,5 +1,29 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    # create user using phone_number & password
+    def create_user(self, phone_number, password, **extra_fields):
+        # check if the phone_number is None or empty string
+        if not phone_number:
+            raise ValueError('The phone number must be set')
+        # create a user model with phone_number and pass the extra_fields
+        user = self.model(phone_number=phone_number, **extra_fields)
+        # set the password to user
+        user.set_password(password)
+        # save the user (specify the database using self._db)
+        user.save(using=self._db)
+        return user
+    
+
+    # create superuser using email & password
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, password, **extra_fields)
+
+
 
 
 
@@ -25,16 +49,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)   
 
+    # override objects manager
+    objects = CustomUserManager()
+
     # change username to phone_number to authenticate with phone_number instead of username
     USERNAME_FIELD = 'phone_number'
     # REQUIRED_FIELDS is only used for superusers not normal users
-    REQUIRED_FIELDS = ['email']
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
 
     def __str__(self):
-        return f'{self.first_name} {self.las}'
+        return f'{self.first_name} {self.last_name}'
 
     # superuser can do anything
-    def has_perm(self, perm, obj = ...):
+    def has_perm(self, perm, obj = None):
         return self.is_superuser
     
     # superuser can see all modules
