afe2ec07a8461aa056d1ec5daaad84cb53e4f30e
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-09-05 00:11:18 +0300

    implement Customer in django admin

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+from .models import Customer
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    # in list_display you can't use related fields such as user__first_name
+    # so you have to define related fields and decorate with @admin.display()
+    @admin.display(ordering="user__first_name", description="First Name")
+    def first_name(self, customer: Customer):
+        return customer.user.first_name
+    
+    @admin.display(ordering="user__last_name", description="Last Name")
+    def last_name(self, customer: Customer):
+        return customer.user.last_name
+    
+    @admin.display(ordering="user__phone_number", description="Phone Number")
+    def phone_number(self, customer: Customer):
+        return customer.user.phone_number
+
+    list_display = ['id', 'first_name', 'last_name', 'phone_number']
+    # define the related fields
+    list_select_related = ['user']
+    ordering = ['user__first_name', 'user__last_name']
+    search_fields = ['user__first_name__istartswith', 'user__last_name__istartswith']
