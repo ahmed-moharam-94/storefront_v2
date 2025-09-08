7ae75dfe2ddfb8423684af81ad84811a6d1f1eb0
ahmedezzat <ahmed.moharam.abuelyazeed@gmail.com>
2025-09-08 14:39:33 +0300

    - Implement update customer

--- store/serializers.py
+++ store/serializers.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'phone_number']
+        read_only_fields = ['id']
 
 
 # list/detail Customer
@@ -22,4 +23,29 @@ class CustomerSerializer(serializers.ModelSerializer):
 
 
 # update customer
-# class UpdateCustomerSerializer(serializers.M )
+class UpdateCustomerSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    class Meta:
+        model = Customer
+        fields = ['user', 'birth_date', 'second_phone_number']
+
+
+    def update(self, instance, validated_data):
+        # get the user object from validated data the remove it
+        user_data = validated_data.pop('user', None)
+
+        # update the user
+        if user_data:
+            user = instance.user
+            # iterate over user_data dictionary items to update the user instance with right values
+            for attr, value in user_data.items():
+                setattr(user, attr, value)
+            user.save()
+
+        # update the customer profile
+        instance = super().update(instance, validated_data)
+
+        return instance
+
+    
+
--- store/views.py
+++ store/views.py
@@ -3,12 +3,18 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from.models import Customer
 from.permissions import IsOwnerOrAdmin
-from.serializers import CustomerSerializer
+from.serializers import CustomerSerializer, UpdateCustomerSerializer
 
 class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.select_related('user').all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAuthenticated]
+
+
+    def get_serializer_class(self):
+        if self.action in ['update', 'partial_update']:
+            return UpdateCustomerSerializer
+        return self.serializer_class
     
     def get_permissions(self):
         if self.action == 'list':
